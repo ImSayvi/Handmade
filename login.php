0aafc5b408aca4b25cc24cffcdabbd4a3bbf6a32
MichalSondej <mic.sondej@gmail.com>
2024-04-21 18:30:11 +0200

    add login function

--- login.php
+++ login.php
@@ -18,12 +18,12 @@ session_start();
         <form method="POST">
             <div class="mb-3">
                 <label for="username" class="form-label">Email address</label>
-                <input type="text" class="form-control" id="username" aria-describedby="emailHelp">
+                <input type="text" class="form-control" name="username" id="username" aria-describedby="emailHelp">
                 <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <input type="password" name="password" class="form-control" id="exampleInputPassword1">
             </div>
             <div class="mb-3 form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
@@ -42,12 +42,15 @@ session_start();
 if (isset($_POST['username'])) {
     $_SESSION['username'] = $_POST['username'];
     $username = $_SESSION['username'];
+    $_SESSION['password'] = $_POST['password'];
+    $password = $_SESSION['password'];
 
-    if ($username = 123) {
+    if ($username == 123 && $password == 123) {
         header("Location: index.php");
     } else {
-        header("Location: index.php");
+        echo "zly login";
     }
 } else {
     echo 'siurekssie';
+    echo var_dump($username);
 }
