3e2b7dbd31d1cca9b70f0a03553f6563ee49d9a2
Say.vi <say.julia.vi@gmail.com>
2024-04-23 21:07:30 +0200

    styling login page

--- login.php
+++ login.php
@@ -10,6 +10,7 @@ require_once('db.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/988d321f51.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style/style.css">
     <title>Document</title>
 </head>
@@ -25,6 +26,7 @@ require_once('db.php');
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label"></label>
                 <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                
             </div>
             <div class="mb-3 form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
